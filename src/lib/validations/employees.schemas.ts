405bde3bdfc3c941d47c5c38239123e3b3d267ae
siwakasen <siwakasen@gmail.com>
2025-09-24 13:46:13 +0700

    fix: char in schema

--- src/lib/validations/employees.schemas.ts
+++ src/lib/validations/employees.schemas.ts
@@ -1,39 +1,39 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const LoginFormSchema = z.object({
   email: z
     .string()
-    .min(1, { message: "Email must be provided" })
-    .email({ message: "Please enter a valid email." })
+    .min(1, { message: 'Email must be provided' })
+    .email({ message: 'Please enter a valid email.' })
     .trim(),
-  password: z.string().min(1, { message: "Password must be provided" }).trim(),
+  password: z.string().min(1, { message: 'Password must be provided' }).trim(),
 });
 
 export const ForgetPasswordFormSchema = z.object({
-  email: z.string().email({ message: "Please enter a valid email." }).trim(),
+  email: z.string().email({ message: 'Please enter a valid email.' }).trim(),
 });
 
 export const ChangePasswordFormSchema = z
   .object({
     password: z
       .string()
-      .min(8, { message: "Password must be at least 8 characters long" })
+      .min(8, { message: 'Password must be at least 8 characters long' })
       .regex(/[a-zA-Z]/, {
-        message: "Password must contain at least one letter.",
+        message: 'Password must contain at least one letter.',
       })
-      .regex(/[0-9]/, { message: "Password must contain at least one number." })
-      .regex(/[!@#$%^&*]/, {
-        message: "Password must contain at least one special character.",
+      .regex(/[0-9]/, { message: 'Password must contain at least one number.' })
+      .regex(/[!@#$%^&*_-]/, {
+        message: 'Password must contain at least one special character.',
       })
       .trim(),
     confirmPassword: z
       .string()
-      .min(1, { message: "Confirm password must be provided" })
+      .min(1, { message: 'Confirm password must be provided' })
       .trim(),
   })
   .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords must be match",
-    path: ["confirmPassword"],
+    message: 'Passwords must be match',
+    path: ['confirmPassword'],
   });
 
 export type LoginFormSchemaType = z.infer<typeof LoginFormSchema>;
@@ -41,35 +41,35 @@ export type ForgetPasswordFormSchemaType = z.infer<
   typeof ForgetPasswordFormSchema
 >;
 export const CreateEmployeeSchema = z.object({
-  name: z.string().min(1, { message: "Name is required" }),
+  name: z.string().min(1, { message: 'Name is required' }),
   email: z
     .string()
-    .min(1, { message: "Email is required" })
-    .email({ message: "Please enter a valid email." })
+    .min(1, { message: 'Email is required' })
+    .email({ message: 'Please enter a valid email.' })
     .trim(),
   password: z
     .string()
-    .min(1, { message: "Password is required" })
-    .min(8, { message: "Password must be at least 8 characters long" })
+    .min(1, { message: 'Password is required' })
+    .min(8, { message: 'Password must be at least 8 characters long' })
     .regex(/[a-z]/, {
-      message: "Password must contain at least one lowercase letter.",
+      message: 'Password must contain at least one lowercase letter.',
     })
     .regex(/[A-Z]/, {
-      message: "Password must contain at least one uppercase letter.",
+      message: 'Password must contain at least one uppercase letter.',
     })
-    .regex(/[0-9]/, { message: "Password must contain at least one number." })
-    .regex(/[!@#$%^&*]/, {
-      message: "Password must contain at least one special character.",
+    .regex(/[0-9]/, { message: 'Password must contain at least one number.' })
+    .regex(/[!@#$%^&*_-]/, {
+      message: 'Password must contain at least one special character.',
     })
     .trim(),
-  role_id: z.number().min(1, { message: "Role is required" }),
-  salary: z.number().min(1, { message: "Salary must be greater than 0" }),
+  role_id: z.number().min(1, { message: 'Role is required' }),
+  salary: z.number().min(1, { message: 'Salary must be greater than 0' }),
 });
 
 export const UpdateEmployeeSchema = z.object({
-  name: z.string().min(1, { message: "Name is required" }),
-  role_id: z.number().min(1, { message: "Role is required" }),
-  salary: z.number().min(1, { message: "Salary must be greater than 0" }),
+  name: z.string().min(1, { message: 'Name is required' }),
+  role_id: z.number().min(1, { message: 'Role is required' }),
+  salary: z.number().min(1, { message: 'Salary must be greater than 0' }),
 });
 
 export type ChangePasswordFormSchemaType = z.infer<
@@ -77,4 +77,3 @@ export type ChangePasswordFormSchemaType = z.infer<
 >;
 export type CreateEmployeeSchemaType = z.infer<typeof CreateEmployeeSchema>;
 export type UpdateEmployeeSchemaType = z.infer<typeof UpdateEmployeeSchema>;
-
